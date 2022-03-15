21c087c5ee985ad758dd5a5daff93cd868958971
Lina Marcela <linavilla.2@gmail.com>
2022-03-15 15:09:08 -0500

    importamos e invocamos la funcion desde el login-controlles

--- src/views/login.js
+++ src/views/login.js
@@ -1,5 +1,7 @@
 // eslint-disable-next-line import/no-cycle
 // import { changeView } from '../routers/router.js';
+import { singupGoogleEvent } from '../controllers/singup.controller.js';
+import { loginBtnEvent } from '../controllers/login.controller.js';
 
 // se crea template de login
 export default () => {
@@ -17,13 +19,26 @@ export default () => {
         <p>---- O ----</p>
         <section class="singup-google">
             <p>Iniciar sesión con</p>
-            <img src="./assets/images/logogoogle.png" width="50px"></img>
+            <img id="login-google" src="./assets/images/logogoogle.png" width="50px"></img>
             <p>¿No tienes cuenta?</p>
             <button type="button" id="singup-btn-load" class="singup-btn-2">Regístrate</button>
         </section>
     </form>`;
   const divElementLogin = document.createElement('div');
   divElementLogin.innerHTML = viewLogin;
+  // se agrega evento click a boton continuar para crear usuario con correo y contraseña
+  const loginBtn = divElementLogin.querySelector('#login-btn');
+  loginBtn.addEventListener('click', () => {
+    const loginEmail = divElementLogin.querySelector('#email').value;
+    const loginPassword = divElementLogin.querySelector('#pass').value;
+    // aca se está creando un usuario y deberia ser autenticado
+    loginBtnEvent(loginEmail, loginPassword);
+  });
+  // se agrega evento click a imagen para autenticar usuario con google
+  const authGoogle = divElementLogin.querySelector('#login-google');
+  authGoogle.addEventListener('click', () => {
+    singupGoogleEvent();
+  });
 
   // se agrega evento click a boton de singup
   const btnRegistrar = divElementLogin.querySelector('#singup-btn-load');
