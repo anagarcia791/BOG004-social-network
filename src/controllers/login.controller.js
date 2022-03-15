7e389198e65c32fd1ffc7e06e32ca4674d9fc8be
Lina Marcela <linavilla.2@gmail.com>
2022-03-15 12:31:09 -0500

    Creación de función para revisión de autenticación

--- src/controllers/login.controller.js
+++ src/controllers/login.controller.js
@@ -1,3 +1,22 @@
-export function xxx() {
+// eslint-disable-next-line import/no-unresolved
+import { getAuth, signInWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/9.6.8/firebase-auth.js';
 
-}
+export const loginBtnEvent = (loginEmail, loginPassword) => {
+  const auth = getAuth();
+  const email = loginEmail;
+  const password = loginPassword;
+  signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+    // Signed in
+      const user = userCredential.user;
+      console.log(user);
+      window.location.hash = '#/wall'; // se cambia ventana cuando el usuario se loguea con su  cuenta
+    // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      console.log(errorCode);
+      const errorMessage = error.message;
+      console.log(errorMessage);
+    });
+};
