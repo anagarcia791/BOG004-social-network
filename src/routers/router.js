9ef9524604a4ae081323cc54b81fb81dfb38dac7
anagarcia791 <amgt2697@gmail.com>
2022-03-14 19:39:43 -0500

    se crea carpeta que contiene manejo de ruteo

---
+++ src/routers/router.js
@@ -0,0 +1,26 @@
+// importamos el componente de las vistas
+import { components } from '../views/index.js';
+
+// funcion de condicion de cambio de rutas
+const changeView = (route) => {
+  console.log(route, 'route desde router.js');
+  const hash = route.split('/')[1];
+  console.log(hash, 'hash desde router.js');
+  const sectionHTML = document.querySelector('#routers-container');
+  sectionHTML.innerHTML = '';
+  switch (route) {
+    case '':
+    case ' ':
+    case '#':
+    case '#/':
+    case 'localhost:3000':
+    { return sectionHTML.appendChild(components.login()); }
+    case '#/singup':
+    case '#/wall':
+    { return sectionHTML.appendChild(components[hash]()); }
+    default:
+      return sectionHTML.appendChild(components.error404());
+  }
+};
+
+export { changeView };
