5773cde90a8025a1d0732aa0d2cde70d37243dc2
anagarcia791 <amgt2697@gmail.com>
2022-03-14 19:36:11 -0500

    se crea funcion escuchador de cambio en hash para ruteo

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
-// Este es el punto de entrada de tu aplicacion
+// importamos los cambios de vistas y funciones que controlan eventos
+import { changeView } from './routers/router.js';
 
-import { myFunction } from './lib/index.js';
+// funcion init para vistas y eventos
+const init = () => {
+  console.log(window.location.hash, 'hi desde funcion init de main.js');
+  changeView(window.location.hash);
+  window.addEventListener('hashchange', () => changeView(window.location.hash));
+};
 
-myFunction();
+window.addEventListener('load', init);
