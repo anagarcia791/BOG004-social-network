134360a15dab389c951dd7c676cf6aa96513c9ed
anagarcia791 <amgt2697@gmail.com>
2022-03-17 17:59:54 -0500

    se elimina archivo por defecto de test

--- test/index.spec.js
+++
@@ -1,8 +0,0 @@
-// importamos la funcion que vamos a testear
-// import { myFunction } from '../src/controller/router';
-
-// describe('myFunction', () => {
-//   it('debería ser una función', () => {
-//     expect(typeof myFunction).toBe('function');
-//   });
-// });
