0bccc8a41dd7a7d8c1a1d674a000b0faf4c129b8
anagarcia791 <amgt2697@gmail.com>
2022-03-17 17:54:17 -0500

    se agrega carpeta para mocks de firebase

---
+++ src/__mocks__/firebase-init.js
@@ -0,0 +1,6 @@
+export const initializeApp = () => Promise.resolve({});
+export const getAuth = () => Promise.resolve({});
+export const createUserWithEmailAndPassword = () => Promise.resolve({});
+export const signInWithPopup = () => Promise.resolve({});
+export class GoogleAuthProvider {}
+export const signInWithEmailAndPassword = () => Promise.resolve({});
