d79c1c82fc4d445f9adb3de893527c0b7015154d
anagarcia791 <amgt2697@gmail.com>
2022-03-21 00:14:18 -0500

    se reestructura función para cambiar las promesas directamente al archivo que contiene la creacion del html

--- src/controllers/signup.controller.js
+++ src/controllers/signup.controller.js
@@ -5,62 +5,17 @@ import {
   signInWithPopup,
   provider,
 } from '../firebase-init.js';
-import { showNotification } from './alerts.controllers.js';
-// import { changeView } from '../router.js'; GoogleAuthProvider,
 
 // funcion para crear usuario con correo y contraseña
 export const signupBtnEvent = (signupEmail, signupPassword) => {
   const auth = getAuth();
   const email = signupEmail;
   const password = signupPassword;
-  createUserWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-      // Signed in
-      const user = userCredential.user;
-      console.log(user);
-      // ...
-      // changeView('#/wall');
-      window.location.hash = '#/wall'; // se cambia ventana cuando crea cuenta
-    })
-    .catch((error) => {
-      // const errorCode = error.code;
-      // console.log(errorCode);
-      const errorMessage = error.message;
-      showNotification(errorMessage);
-      // ..
-    });
+  return createUserWithEmailAndPassword(auth, email, password);
 };
 
 // funcion para crear usuario con google
 export const signupGoogleEvent = () => {
-  // se declara como nuestro proveedor a google
   const auth = getAuth();
-  // const provider = new GoogleAuthProvider();
-  signInWithPopup(auth, provider)
-    .then((result) => {
-      // This gives you a Google Access Token. You can use it to access the Google API.
-      // const credential = GoogleAuthProvider.credentialFromResult(result);
-      // console.log(credential);
-      console.log(result);
-      // const token = credential.accessToken;
-      // The signed-in user info.
-      // const user = result.user;
-      // ...
-      // changeView('#/wall');
-      // window.location.href = '#/wall'
-      window.location.hash = '#/wall'; // se cambia ventana cuando autentica cuenta
-    }).catch((error) => {
-      // Handle Errors here.
-      // const errorCode = error.code;
-      // console.log(errorCode);
-      const errorMessage = error.message;
-      console.log(errorMessage);
-      // The email of the user's account used.
-      // const email = error.email;
-      // console.log(email);
-      // The AuthCredential type that was used.
-      // const credential = GoogleAuthProvider.credentialFromError(error);
-      // console.log(credential);
-    // ...
-    });
+  return signInWithPopup(auth, provider);
 };
