cbfb4dcc0e25bf8b37d4d775b4ae02ad13b91a0d
anagarcia791 <amgt2697@gmail.com>
2022-03-21 00:20:42 -0500

    se agrega funcion para crear la lista de botones que permiten publicar segun el genero musical de preferencia

--- src/views/wall.js
+++ src/views/wall.js
@@ -6,30 +6,53 @@ export default () => {
         <nav class="wall-nav">
             <img src="./assets/images/logobemusic.png" alt="logoBeMusic"></img>
             <picture class="wall-nav-pic">
-                <i class="fa-solid fa-user"></i>
+                <i class="fa-solid fa-user" id="userpic"></i>
                 <i class="fa-solid fa-arrow-right-from-bracket" id="signout" ></i>
             </picture>
         </nav>
     </header>
     <section class="wall-categories">
         <h2 class="wall-categ-title">Publicar</h2>
-        <div class="wall-categ-container">
-            <a href="">Rock</a>
-            <a href="">Pop</a>
-        </div>
+        <section class="wall-categ-container" id="wall-categ-container"></section>
     </section>
     <main class="wall-posts">
-        <section class="wall-posts-container"></section>
+        <section class="wall-posts-container">
+            <p>aca van las publicaciones</p>
+        </section>
     </main>
   </section>`;
 
   const divElementWall = document.createElement('div');
   divElementWall.innerHTML = wall;
 
+  const musicCategories = {
+    type: ['Rock', 'Pop', 'Romántica', 'Reggaeton', 'Electrónica', 'Bailable', 'Country', 'Salsa'],
+  };
+
+  // funcion para crear estructura de filtros en html
+  const createCategoriesStructure = (mCategories) => {
+    // declaracion variable para agregar generos musicales
+    const musicCategoriesSec = divElementWall.querySelector('#wall-categ-container');
+    let musicValues = '';
+    // ciclo para crear a ref o botones de generos musicales
+    mCategories.forEach((value, index) => {
+      musicValues += `<button type="button" id="${value}${index}">${value}</button>`;
+      // musicValues += `<a href="" class="wall-categ-genre" id="${value}${index}">${value}</a>`;
+    });
+    // inserta estructura filtros
+    musicCategoriesSec.innerHTML = musicValues;
+  };
+
+  // se invoca a la funcion createCategoriesStructure para crear opcion de publicacion
+  createCategoriesStructure(musicCategories.type);
+
+  // se camptura avatar
+  const userPic = divElementWall.querySelector('#userpic');
+  console.log(userPic);
+
   // se agrega evento click a boton de login
   const signoutBtn = divElementWall.querySelector('#signout');
   signoutBtn.addEventListener('click', () => {
-    // event.preventDefault(); // changeView('#/');
     window.location.href = '#/'; // se cambia ventana a login para iniciar sesion
   });
 
