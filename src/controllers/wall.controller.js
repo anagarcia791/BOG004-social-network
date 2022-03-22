d63def741e425f1c21e79c566a830bfc283d3fee
Lina Marcela <linavilla.2@gmail.com>
2022-03-22 18:43:20 -0500

    se realiza función para capturar url de la foto con google y otra función para logout

--- src/controllers/wall.controller.js
+++ src/controllers/wall.controller.js
@@ -1,31 +1,17 @@
 // se importan funciones de firebase y de notificacion
-import { getAuth } from '../firebase-init.js';
+import { getAuth, signOut } from '../firebase-init.js';
 
 // funcion para estado de usuario
-export const stateChangeViewer = () => {
+export const googlePhoto = () => {
   const auth = getAuth();
   const user = auth.currentUser;
-  let photoURL;
-  if (user !== null) {
-    // The user object has basic properties such as display name, email, etc.
-    // displayName = user.displayName;
-    // const email = user.email;
-    photoURL = user.photoURL;
-    // // The user's ID, unique to the Firebase project. Do NOT use
-    // // this value to authenticate with your backend server, if
-    // // you have one. Use User.getToken() instead.
-    // const uid = user.uid;
-  }
-  return photoURL;
-  // onAuthStateChanged  // onAuthStateChanged(auth, (user) => {
-  //   if (user) {
-  //     // User is signed in, see docs for a list of available properties
-  //     const uid = user.uid;
-  //     console.log(uid);
-  //     // ...
-  //   } else {
-  //     // User is signed out
-  //     // ...
-  //   }
-  // });
+  return user.photoURL;
+};
+
+export const stateChangeViewer = () => {
+  const auth = getAuth();
+  const signOutEvent = signOut(auth);
+  const user = auth.currentUser;
+  console.log(user, 'hola estoy tratando de desloguearme');
+  return signOutEvent;
 };
