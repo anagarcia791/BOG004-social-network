eab8e90b8da77bcd8df55e9f6eea0ce721d25ff5
Karen Alarcon <akareny.10@gmail.com>
2022-03-24 13:04:36 -0500

    se trato de exportar funcion para persistencia desde firebase

--- src/controllers/wall.controller.js
+++ src/controllers/wall.controller.js
@@ -1,12 +1,27 @@
 // se importan funciones de firebase y de notificacion
-import { getAuth, signOut } from '../firebase-init.js';
+import {
+  getAuth,
+  signOut,
+  // setPersistence,
+  // browserSessionPersistence,
+} from '../firebase-init.js';
 
 // funcion para estado de usuario
 export const googlePhoto = () => {
   const auth = getAuth();
   const user = auth.currentUser;
+  if (user == null) {
+    console.log('no encontro el ususuario', user);
+    window.location.hash = '#/';
+    return 1;
+  }
   return user.photoURL;
 };
+
+// export const persistence = () => {
+//   const auth = getAuth();
+//   return setPersistence(auth, browserSessionPersistence);
+// };
 
 export const stateChangeViewer = () => {
   const auth = getAuth();
