36c5eeb8cdf65c9c43628148b582a091de02573b
anagarcia791 <amgt2697@gmail.com>
2022-03-30 12:22:16 -0500

    se agrega observador para el usuario en muro

--- src/controllers/wall.controller.js
+++ src/controllers/wall.controller.js
@@ -10,8 +10,33 @@ import {
   onSnapshot,
 } from '../firebase-init.js';
 
+// funcion observador fanny
+export const watcher = () => {
+  const auth = getAuth();
+  let watcherUser;
+  let uid;
+  onAuthStateChanged(auth, (user) => {
+    if (user === null || user === undefined) {
+      window.location.hash = '#/';
+      watcherUser = null;
+    } else {
+      watcherUser = user;
+      uid = user.uid;
+      window.location.hash = '#/wall';
+    }
+  });
+  console.log(watcherUser, 'watcher usuario wall controller');
+  console.log(uid, 'watcher id wall controller');
+  const currentUserResult = auth.currentUser;
+  console.log(currentUserResult, 'currentUserResult usuario wall controller');
+  return watcherUser;
+};
+
 // funcion para conseguir info user
 export const currentUser = () => {
+  const watcherResult = watcher();
+  console.log(watcherResult, 'watcherResult usuario wall controller');
+
   const auth = getAuth();
   const user = auth.currentUser;
   if (user === null) {
@@ -29,40 +54,17 @@ export const signOutUser = () => {
   return signOutEvent;
 };
 
-// funcion observador fanny
-export const watcher = () => {
-  const auth = getAuth();
-  onAuthStateChanged(auth, (user) => {
-    let uid;
-    if (user) {
-      uid = user.uid;
-      console.log(uid, 'id del usuario');
-    } else {
-      uid = null;
-    }
-    return uid;
-  });
-};
-
-// export const observador = () => {
-//   const auth = getAuth();
-//   return onAuthStateChanged(auth, (user) => user);
-// };
-
 // Función para crear publicación
 export const createPublication = (inputPost, genere) => {
-  console.log(inputPost, genere);
   addDoc(collection(db, 'publications'), { inputPost, genere });
 };
 
 // Función para leer publicación
 export const readPublication = () => {
-  console.log('lista de publicaciones');
-  return getDocs(collection(db, 'publications'));
+  getDocs(collection(db, 'publications'));
 };
 
-// Función para leer publicación
+// Función para leer todas las publicación
 export const onReadPublication = (querySnapshot) => {
-  console.log('onReadPublication');
-  return onSnapshot(collection(db, 'publications'), querySnapshot);
+  onSnapshot(collection(db, 'publications'), querySnapshot);
 };
