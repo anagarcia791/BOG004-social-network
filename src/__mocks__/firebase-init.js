68c89b4ab899b749fc0c88e6ac32dd809dd7079c
Lina Marcela <linavilla.2@gmail.com>
2022-04-01 13:24:57 -0500

    se crea mok de Crear usuario con email y contraseña para el test

--- src/__mocks__/firebase-init.js
+++ src/__mocks__/firebase-init.js
@@ -1,6 +1,35 @@
+// Funciones de Firebase
 export const initializeApp = () => Promise.resolve({});
 export const getAuth = () => Promise.resolve({});
-export const createUserWithEmailAndPassword = () => Promise.resolve({});
 export const signInWithPopup = () => Promise.resolve({});
 export class GoogleAuthProvider {}
 export const signInWithEmailAndPassword = () => Promise.resolve({});
+export const onAuthStateChanged = () => Promise.resolve({});
+export const signOut = () => Promise.resolve({});
+export const setPersistence = () => Promise.resolve({});
+
+export const createUserWithEmailAndPassword = jest.fn((email, password) => {
+  const userCredentials = {
+    user: { uid: '123' },
+  };
+  const error = {
+    status: false,
+    code: 'auth/email-already-in-use',
+  };
+  let promiseResult;
+  if (email === 'ana1@gmail.com' && password === 'ana12345') {
+    promiseResult = Promise.reject(error);
+  } else {
+    promiseResult = Promise.resolve(userCredentials);
+  }
+  return promiseResult;
+});
+
+// Funciones de Firestore
+export const getFirestore = () => Promise.resolve({});
+export const collection = () => Promise.resolve({});
+export const addDoc = () => Promise.resolve({});
+export const getDocs = () => Promise.resolve({});
+export const onSnapshot = () => Promise.resolve({});
+export const deleteDoc = () => Promise.resolve({});
+export const doc = () => Promise.resolve({});
